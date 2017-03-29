56161b2e9e132a2fdcb88e8f7fea46be41263ba9
STUDENT0027 <STUDENT0027@users.noreply.github.com>
2017-03-29 11:44:06 -0400

    Lab8

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,47 @@
 #include "ofApp.h"
+#include <random>
+using namespace std;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	/*//uniform
+	int minx=0 , miny = 0;
+	int maxy=1200 , maxx = 970;
+	
+	random_device rd;
+	mt19937 en(rd());
+	
+	uniform_int_distribution <int> my_disk(minx,maxx);
+	uniform_int_distribution <int> my_lisk(miny,maxy);
+	
+	for(int i=0;i<20;i++)
+	X.push_back (my_disk(en));
+	
+	for(int i=0;i<20;i++)
+	Y.push_back (my_lisk(en));*/
+	
+	
+	float meanx=1000.0f , sd =15.0f , meany= 800.0f;
+	random_device rd;
+	mt19937 en(rd());
+	normal_distribution <> my_disk (meanx,sd);
+	normal_distribution <> my_lisk (meany,sd);
+	
+	for(int i=0;i<20;i++)
+	X.push_back (my_disk(en));
+	
+	for(int i=0;i<20;i++)
+	Y.push_back (my_lisk(en));
+	
+	
+	
+	
+	
+	
+
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+//X = { };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
